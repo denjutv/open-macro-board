9444e8605ef99012a1b7a891cdff9dce33e5411b
denjutv <info@denju.tv>
2019-02-26 22:44:15 +0100

    added some comments

--- render/src/reducer/button.js
+++ render/src/reducer/button.js
@@ -1,5 +1,11 @@
 import { REQUEST_BUTTON_SETTINGS } from "../action";
 
+/**
+ * Handles all events, that change the buttons.
+ * 
+ * @param {Object} state Current state passed by the redux store
+ * @param {Object} action Current action object
+ */
 const buttonReducer = ( state = null, action ) =>
 {
     switch( action.type )
