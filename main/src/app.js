f4a1d65d4b3e7793270696511637471ee73a4f28
denjutv <info@denju.tv>
2019-03-13 22:01:13 +0100

    cleanup of init method

--- main/src/app.js
+++ main/src/app.js
@@ -11,27 +11,16 @@ class App
 
     init()
     {
-        // init config
-        const defaultConfig = require( "./defaultConfig" );
-        const Configstore = require( "configstore" );
- 
-        // create a Configstore instance with an unique ID and default values
-        this.conf = new Configstore( "open-macro-board", defaultConfig);
-
-        // init redux store
-        const { createStore, applyMiddleware } = require( "redux" );
-        const rootReducer = require( "./reducer" );
-        const buttonMiddleare = require( "./middleware/redux/button" );
-        const initialState = {buttons: this.conf.get("buttons")};
-        this.store = createStore( rootReducer, initialState, applyMiddleware( buttonMiddleare ) );
-
-
-        // create window
-        const mainWindow = require( "./mainWindow" );
-
         this.parseAguments();
 
-        this.app.on( "ready", () => mainWindow.create( this.conf.get("display"), this.isDev ) );
+        // init config
+        this.initConfig();
+
+        // init redux store
+        this.initStore();
+
+        // create window
+        this.initMainWindow();
 
         this.app.on( "window-all-closed", () =>
         {
@@ -41,24 +30,10 @@ class App
             }
         });
 
-        this.app.on( "activate", () => {
-            if( mainWindow.win === null ) {
-                createWindow();
-            }
-        });
-
         // ipc
-        const { ipcMain } = require( "electron" );
-        const { MAIN_RENDER_CHANNEL } = require( "../../shared/channel" );
-        const { REQUEST_BUTTON_SETTINGS } = require( "../../shared/actionType" );
-
-        // handle ipc message by dispatching them to the store
-        ipcMain.on( MAIN_RENDER_CHANNEL, ( event, message ) =>
-        {
-            this.store.dispatch( Object.assign( message, {event} ) );
-        });
+        this.initIpc();
     }
-
+    
     parseAguments()
     {
         if( process.argv.includes( "-dev" ) )
@@ -66,6 +41,50 @@ class App
             this.isDev = true;
             console.log( "run in dev mode" );
         }
+    }
+
+    initConfig()
+    {
+        const defaultConfig = require( "./defaultConfig" );
+        const Configstore = require( "configstore" );
+ 
+        // create a Configstore instance with an unique ID and default values
+        this.conf = new Configstore( "open-macro-board", defaultConfig);
+    }
+
+    initStore()
+    {
+        const { createStore, applyMiddleware } = require( "redux" );
+        const rootReducer = require( "./reducer" );
+        const buttonMiddleare = require( "./middleware/redux/button" );
+        const initialState = {buttons: this.conf.get("buttons")};
+        this.store = createStore( rootReducer, initialState, applyMiddleware( buttonMiddleare ) );
+    }
+
+    initMainWindow()
+    {
+        this.mainWindow = require( "./mainWindow" );
+
+        this.app.on( "ready", () => this.mainWindow.create( this.conf.get("display"), this.isDev ) );
+
+        this.app.on( "activate", () => {
+            if( this.mainWindow.win === null ) {
+                // createWindow();
+                this.mainWindow.create( this.conf.get("display"), this.isDev );
+            }
+        });
+    }
+
+    initIpc()
+    {
+        const { ipcMain } = require( "electron" );
+        const { MAIN_RENDER_CHANNEL } = require( "../../shared/channel" );
+
+        // handle ipc message by dispatching them to the store
+        ipcMain.on( MAIN_RENDER_CHANNEL, ( event, message ) =>
+        {
+            this.store.dispatch( Object.assign( message, {event} ) );
+        });
     }
 };
 
