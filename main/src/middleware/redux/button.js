efb209bef203226d1adfc1c5cdfdf9fd32920ac6
denjutv <info@denju.tv>
2020-01-14 22:15:27 +0100

    implemented sendRequest for HTTP macro

--- main/src/middleware/redux/button.js
+++ main/src/middleware/redux/button.js
@@ -11,14 +11,21 @@ const buttonMiddleware = ( { getState, dispatch } ) =>
     return ( next ) => (action) => 
     {
         let result = null;
+        const state = getState();
 
         switch( action.type )
         {
             case REQUEST_BUTTON_SETTINGS:
-                action.event.sender.send( MAIN_RENDER_CHANNEL, deliverButtonSettings( getState().buttons ) );
+                action.event.sender.send( MAIN_RENDER_CHANNEL, deliverButtonSettings( state.buttons ) );
             break;
             case BUTTON_PRESSED:
-                app.connectionManager.broadcast( {type: BUTTON_PRESSED, buttonIndex: action.buttonIndex } );
+                if( state.buttons[action.buttonIndex] && state.buttons[action.buttonIndex].macroType === "httpRequest" )
+                {
+                    sendRequest( state.buttons[action.buttonIndex].macro );
+                }
+                else {
+                    app.connectionManager.broadcast( {type: BUTTON_PRESSED, buttonIndex: action.buttonIndex } );
+                }
             break;
             // store button settings in config and send it to the render process
             case BUTTONS_UPDATE:
@@ -32,5 +39,68 @@ const buttonMiddleware = ( { getState, dispatch } ) =>
         return result;
     };
 };
+
+async function sendRequest( macro )
+{
+    const axios = require( "axios" );
+    let response = null;
+    const data = macro.body === "plain" ? macro.plainBody : buildQueryString( macro.argument );
+    const config = {};
+
+    console.log(macro);
+
+    if( macro.header && macro.header.length )
+    {
+        config.headers = buildHeaderObj( macro.header );
+    }
+
+    switch( macro.method )
+    {
+        case "get":
+            response = await axios.get( macro.url, config );
+            break;
+        case "post":
+            response = await axios.post( macro.url, data, config );
+            break;
+        case "put":
+            response = await axios.put( macro.url, data, config );
+            break;
+        case "delete":
+            response = await axios.delete( macro.url, config );
+            break;
+    }
+
+    console.log(response);
+}
+
+function buildQueryString( argumentList )
+{
+    const queryParts = [];
+
+    argumentList.forEach( argument =>
+    {
+        if( argument.checked )
+        {
+            queryParts.push( argument.key + "=" + argument.value );
+        }
+    });
+
+    return "?" + queryParts.join("&");
+}
+
+function buildHeaderObj( headerList )
+{
+    const headerObj = {};
+
+    headerList.forEach( header =>
+    {
+        if( header.checked )
+        {
+            headerObj[ header.key ] = header.value;
+        }   
+    });
+
+    return headerObj;
+}
 
 module.exports = buttonMiddleware;
